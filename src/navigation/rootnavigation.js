de4a7b3c29bba0bf295683577afa76b31435cebe
TXT-80 <txt.hackathon@TXTs-MacBook-Pro-30.local>
2023-07-24 11:04:43 -0700

    Camera

--- src/navigation/rootnavigation.js
+++ src/navigation/rootnavigation.js
@@ -1,6 +1,6 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
-import ScheduleScreen from "../screens/ScheduleScreen";
+import RecordScreen from "../screens/RecordScreen";
 import GymStack from "./GymStack";
 import ProfileScreen from "../screens/ProfileScreen";
 
@@ -9,7 +9,7 @@ export default function RootNavigation() {
   return (
     <Tab.Navigator initialRouteName="Home">
       <Tab.Screen name="Home" component={GymStack} options={{headerShown: false}}/>
-      <Tab.Screen name="GymSchedule" component={ScheduleScreen} />
+      <Tab.Screen name="+" component={RecordScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen}/>
     </Tab.Navigator>
   );
