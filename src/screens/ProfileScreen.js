df17cf1cc21e156ece9bbd3eee6fe879767a7eea
TXT-80 <txt.hackathon@TXTs-MacBook-Pro-30.local>
2023-07-25 18:11:58 -0700

    More Profile Tweaks v1.1

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,37 +1,46 @@
 import { StyleSheet, View, Text, Image, Pressable } from "react-native";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 export default function ProfileScreen({ navigation }) {
   const [show, setShow] = useState(false);
   return (
-      <View style={styles.container}>
-        <View style={styles.editbar}>
-          <Pressable onPress={() => {
+    <View style={styles.container}>
+      <View style={styles.editbar}>
+        <Pressable
+          onPress={() => {
             navigation.navigate("EditProfile");
-          }}>
-            <Image
-            style={styles.edit}
-            source={require('../../assets/edit-button.png')}/>
-          </Pressable>
-        </View>
-        <Image
-          style={styles.img}
-          source={{
-            uri: "https://static.vecteezy.com/system/resources/previews/005/544/718/original/profile-icon-design-free-vector.jpg",
           }}
-        />
-        <Text style={styles.name}>Name</Text>
-        <View style={styles.info}>
+        >
+          <Image
+            style={styles.edit}
+            source={require("../../assets/edit-button.png")}
+          />
+        </Pressable>
+      </View>
+      <Image
+        style={styles.img}
+        source={{
+          uri: "https://static.vecteezy.com/system/resources/previews/005/544/718/original/profile-icon-design-free-vector.jpg",
+        }}
+      />
+      <View style={styles.nameContainer}>
+        <Text style={styles.name}>Fabrizio Bernal</Text>
+      </View>
+      <View style={styles.info}>
+        <View style={styles.ageContainer}>
           <Text style={styles.age}>17 Years Old</Text>
+        </View>
+        <View style={styles.maContainer}>
           <Text style={styles.ma}>Boxing</Text>
         </View>
-        <View>
-          <Text style={styles.exp}>7 Years of experience</Text>
-        </View>
-        <View style={styles.bio}>
-          <Text style={styles.descr}>Bio </Text>
-        </View>
-      </View> 
+      </View>
+      <View style={styles.expContainer}>
+        <Text style={styles.exp}>7 Years of experience</Text>
+      </View>
+      <View style={styles.descr}>
+        <Text style={styles.bio}>Bio </Text>
+      </View>
+    </View>
   );
 }
 const styles = StyleSheet.create({
@@ -51,35 +60,53 @@ const styles = StyleSheet.create({
   edit: {
     height: 50,
     width: 50,
-    alignSelf: 'flex-end'
+    alignSelf: "flex-end",
   },
   name: {
-    display: 'flex',
+    fontFamily: "workSans",
+    fontSize: 20,
+    fontStyle: "normal",
+    fontWeight: 500,
+    lineHeight: 24,
+    letterSpacing: 0.2,
+    color: "#AFB1B6",
+  },
+  nameContainer: {
+    display: "flex",
     width: 340,
     height: 64,
-    padding: 13,
+    paddingVertical: 13,
     paddingHorizontal: 12,
-    alignItems: 'center',
+    justifyContent: "center",
     gap: 10,
     flexShrink: 0,
     borderRadius: 10,
-    borderWidth: 1, 
-    borderStyle:'solid',
-    borderColor: '#AFB1B6'
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#AFB1B6",
+  },
+  expContainer: {
+    display: "flex",
+    width: 340,
+    height: 64,
+    paddingVertical: 13,
+    paddingHorizontal: 12,
+    alignItems: "center",
+    gap: 10,
+    flexShrink: 0,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#AFB1B6",
   },
   exp: {
-    display: 'flex',
-    width: 340,
-    height: 64,
-    padding: 13,
-    paddingHorizontal: 12,
-    alignItems: 'center',
-    gap: 10,
-    flexShrink: 0,
-    borderRadius: 10,
-    borderWidth: 1, 
-    borderStyle:'solid',
-    borderColor: '#AFB1B6'
+    fontFamily: "workSans",
+    fontSize: 20,
+    fontStyle: "normal",
+    fontWeight: 500,
+    lineHeight: 24,
+    letterSpacing: 0.2,
+    color: "#AFB1B6",
   },
   img: {
     width: 200,
@@ -92,54 +119,78 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     backgroundColor: "white",
     width: 340,
-    alignContent: 'center',
-    justifyContent: "space-between"
+    alignContent: "center",
+    justifyContent: "space-between",
+    paddingVertical: 13,
   },
-  age: {
-    display: 'flex',
+  ageContainer: {
+    display: "flex",
     width: 150,
     height: 64,
     padding: 13,
     paddingVertical: 12,
-    alignItems: 'center',
+    alignItems: "center",
     gap: 10,
     flexShrink: 0,
     borderRadius: 10,
-    borderWidth: 1, 
-    borderStyle:'solid',
-    borderColor: '#AFB1B6',
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#AFB1B6",
     fontSize: 16,
   },
-  ma: {
-    display: 'flex',
+  age: {
+    fontFamily: "workSans",
+    fontSize: 20,
+    fontStyle: "normal",
+    fontWeight: 500,
+    lineHeight: 24,
+    letterSpacing: 0.2,
+    color: "#AFB1B6",
+  },
+  maContainer: {
+    display: "flex",
     width: 150,
     height: 64,
     padding: 13,
-    paddingVertical: 12,
-    alignItems: 'center',
+    paddingVertical: 13,
+    alignItems: "center",
     gap: 10,
     flexShrink: 0,
     borderRadius: 10,
-    borderWidth: 1, 
-    borderStyle:'solid',
-    borderColor: '#AFB1B6'
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#AFB1B6",
+  },
+  ma: {
+    fontFamily: "workSans",
+    fontSize: 20,
+    fontStyle: "normal",
+    fontWeight: 500,
+    lineHeight: 24,
+    letterSpacing: 0.2,
+    color: "#AFB1B6",
   },
   bio: {
-   
+    fontFamily: "workSans",
+    fontSize: 20,
+    fontStyle: "normal",
+    fontWeight: 500,
+    lineHeight: 24,
+    letterSpacing: 0.2,
+    color: "#AFB1B6",
   },
   descr: {
-    display: 'flex',
+    display: "flex",
     width: 340,
     height: 165,
-    paddingVertical: 13,
     paddingHorizontal: 12,
-    alignItems: 'flexStart',
+    alignItems: "flexStart",
     gap: 10,
     flexShrink: 0,
     borderRadius: 10,
-    borderWidth: 1, 
-    borderStyle:'solid',
-    borderColor: '#AFB1B6'
-
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#AFB1B6",
+    color: "red",
   },
 });
